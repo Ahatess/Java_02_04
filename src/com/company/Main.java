40d8f65611459c239e884f27196bbd4849d6b111
Andrius <andriuscere@gmail.com>
2018-01-30 15:23:09 +0200

    Uzduotis4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Irasykite 2 skaicius");
+	Scanner sc = new Scanner(System.in);
+	float a = sc.nextFloat();
+	float b = sc.nextFloat();
+	suma(a,b);
+	skirtumas(a,b);
+        float suma = a + b;
+        float skirtumas = a - b;
+    }
+    public static void suma (float a, float b){
+        System.out.println("Suma: " + (a + b));
+    }
+    public static void skirtumas (float a, float b){
+        System.out.println("Skirtumas: " + (a-b));
     }
 }
